cfe4142c85f75302f9b9a006ab6758d38877aeec
svchost32 <hackest123@gmail.com>
2021-07-30 13:06:49 +0800

    730-新增改异步性能改进

--- vue_node/utilities/translate.js
+++ vue_node/utilities/translate.js
@@ -53,12 +53,30 @@ function genderRender(lists) {
 //出生日期翻译
 function birthRender(lists) {
     for (keys in lists) {
-        let content = lists[keys].CSRQ 
-        lists[keys].CSRQ = content.substring(0,4)+'-'+content.substring(4,6)+'-'+content.substring(6,8);
+        let content = lists[keys].CSRQ
+        lists[keys].CSRQ = content.substring(0, 4) + '-' + content.substring(4, 6) + '-' + content.substring(6, 8);
     }
 }
 
+async function insertTrans(list) {
+    transTable.departTrans = await mysql.query(`SELECT * FROM t_depart where BMMC='${list.YHBM}';`)
+    transTable.genderTrans = await mysql.query(`SELECT * FROM ts_bzdm where MC='${list.YHXB}';`)
+
+    insert(list)
+    
+    return list
+}
+
+function insert(list) {
+    list.YHBM = transTable.departTrans[0].BMDM;
+    list.YHXB = transTable.genderTrans[0].CODE;
+    list.CSRQ = list.CSRQ.substring(0, 4) + list.CSRQ.slice(5, 7) + list.CSRQ.slice(8, 10)
+    return list
+  }
+
+
 
 module.exports = {
-    listTrans
+    listTrans,
+    insertTrans
 }
